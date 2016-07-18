dfcac644df55c9ff7c01cd71f5af5b158b0a80bb
Anton Barkovsky <anton@swarmer.me>
2016-07-19 00:40:09 +0300

    Delete docker machinery

--- docker/zip_source.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-APP_NAME=blurry
-branch=$(git rev-parse --abbrev-ref HEAD)
-
-cd ..
-git archive --format zip --prefix "${APP_NAME}/" --output docker/build/${APP_NAME}.zip \
-    "$branch"
